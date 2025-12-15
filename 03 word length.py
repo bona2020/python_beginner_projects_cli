138914703763c6882c3c5e0afaf76ae2aae33dc9
bona2020 <bona@infonix.net>
2025-12-16 04:44:39 +0530

    .\03 word length.py added to the repo

---
+++ 03 word length.py
@@ -0,0 +1,7 @@
+b = input('please enter your name :  ')
+print (b)
+
+a = input('please enter a word or phrase you want to know its length ...')
+print ('hi ', b , 'here is the length of the input provided ', len(a))
+import time
+time.sleep(6)
