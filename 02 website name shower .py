d8a1bd1385c3e344f52f1f46ddd6a48ca0e6eba5
bona2020 <bona@infonix.net>
2025-12-16 04:43:57 +0530

    .\02 website name shower .py add

---
+++ 02 website name shower .py
@@ -0,0 +1,7 @@
+name = input('insert your name please : ')
+print ('hello ' , name , 'how are you today!')
+website1 = input ('please insert website url [https type] to get you webist name: ')
+slice = slice(8,-4)
+print ('hello  ['+ name + '] here is the website name:--->  '+ website1[slice])
+import time
+time.sleep(6)
